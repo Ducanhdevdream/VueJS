a6d3f05e6fac5f6c92768fdbd1a9b293e943d8a5
Ducanhdevdream <nguyenducanh2142@gmail.com>
2021-09-27 18:06:54 +0700

    bai1

---
+++ hello-world/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+    <HW1 msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HW1 from './components/HW1.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HW1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/HW1.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+    <div class="loginWrap">
+      <div class="inputWrap">
+        <div class="inputLabel">Nhập tiêu đề</div>
+        <input type="text" v-model= "title">
+      </div>
+      <div class="inputWrap">
+        <div class="inputLabel">Nhập nội dung</div>
+        <textarea class="inputarea" v-model="areaValue"></textarea><br>
+      </div>
+    </div>
+    <div class="infoWrap">
+
+        <div class="infoLabel"> {{ title }}</div>
+        <div class="infoarea">
+          <span v-html="areaValue"></span>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Xin Chao",
+      areaValue: ''
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+
+  .loginWrap {
+    background: #f5f5f5;
+    border: 1px solid #6e6e6e;
+    padding: 24px;
+    height: 250px;
+    width: 300px;
+
+    .inputWrap {
+      margin-bottom: 12px;
+
+      .inputLabel {
+        font-weight: bold;
+        margin-bottom: 8px;
+        text-align: left;
+      }
+
+      input {
+        width: 292px;
+        height: 30px;
+      }
+
+      .inputarea {
+        width: 292px;
+        height: 130px;
+      }
+    }
+  }
+
+  .infoWrap {
+    display: inline;
+    background: #f5f5f5;
+    border: 1px solid #6e6e6e;
+    padding: 24px;
+    height: 250px;
+    width: 300px;
+    .infoLabel{
+       font-weight: bold;
+        margin-bottom: 8px;
+        text-align: left; 
+    }
+    .infoarea{
+        width: 292px;
+        height: 130px;
+    }
+    
+  }
+}
+</style>
